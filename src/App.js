3ccc981f085b538de909950d3fbb5db9cd757481
taewookimfardark <2012142087@yonsei.ac.kr>
2018-09-16 13:36:14 +0900

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
-<<<<<<< HEAD
-import LogIn from './LogIn';
-=======
 import Login from './Login.js';
->>>>>>> 40fc66c221cac68a2d00036bd411b293f6379561
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        <LogIn/>
-=======
         <Login/>
->>>>>>> 40fc66c221cac68a2d00036bd411b293f6379561
       </div>
     );
   }
