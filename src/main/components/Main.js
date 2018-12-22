70651e2911160982530595c7e31f5b7868b1d200
taewookimfardark <2012142087@yonsei.ac.kr>
2018-12-22 13:23:02 +0900

    .

--- src/main/components/Main.js
+++ src/main/components/Main.js
@@ -7,8 +7,43 @@ import './Main.scss';
 class Main extends React.PureComponent {
 
   render() {
+    const today = new Date();
+    const year = today.getFullYear();
+
+    const month = today.getMonth() + 1;
+    const firstDayOfMonth = (new Date(year, month - 1, 1)).getDay();
+    const daysOfMonth = (new Date(year, month, 0)).getDate();
+
+    const total = firstDayOfMonth + daysOfMonth;
+
+    let row;
+    if(total > 28) row = 5;
+    else row = 4;
+
     return <div className="main">
-      Main
+      <div className="calendar">
+        <div className="calendar-header">
+          {year} {month}
+        </div>
+        <div className="calendar-body">
+          {
+            _.map(_.range(row), (r, i) => {
+              return <div className="calendar-row" key={i}>
+                  {
+                    _.map(_.range(7), (d, i) => {
+                      const day = 7*r + d - firstDayOfMonth + 1;
+                      return <div className="calendar-day">
+                        {
+                          day > 0 && day <= daysOfMonth ? day : ''
+                        }
+                      </div>
+                    })
+                  }
+              </div>
+            })
+          }
+        </div>
+      </div>
     </div>
   }
 }
